3082e21bc05ddcdf221dd2be81ea2200a681f627
gagersommerville <gagersommerville@gmail.com>
2025-10-29 20:44:38 -0500

    Finsished modifying the users resource end point

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+let factor = 2;
+let current = 25;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  current = current * factor;
+  res.send(`Current is: ${current}`);
 });
 
 module.exports = router;
